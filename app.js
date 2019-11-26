272a32e284a0dd5f770a5cd7866b983928a2fc9c
EC2 Default User <ec2-user@ip-172-31-31-17.us-east-2.compute.internal>
2019-11-26 22:28:24 +0000

    late update

--- app.js
+++ app.js
@@ -2,16 +2,87 @@ const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public")); //access images, css, js
-
+/* global $ */
 app.use(express.json());
 
 //routes
 app.get("/", function(req, res){//can also be .use 
+let score = 0; 
+    let f1, f2, f3, f4, f5; 
+    f1=f2=f3=f4=f5="wrong"; 
+    let rando = Math.floor(Math.random() * 3);
+    let array = ["Rhode Island", "Maryland", "Maine", "New York"];
+    let questionArray = array[rando]; 
+    //console.log(req.query.q1); 
     
-    res.render("index");
+    res.render("index", { //passes scores into the view 
+        "score": score,
+        "query": req.query, 
+        "f1":f1,
+        "f2":f2,
+        "f3":f3,
+        "f4":f4,
+        "f5":f5, 
+        "questionArray" : questionArray
+    });
     
-} );
+});
+ app.get("/gradeQuiz", function(req, res){
+     let score = 0; 
+    let f1, f2, f3, f4, f5; 
+    f1=f2=f3=f4=f5="wrong"; 
+     if(req.query.q1 == "sacramento")
+    {
+        score +=  12.5; 
+        f1 = "You got it!"; 
+        // rightAnswer2(1); 
+    }
+    if(req.query.q2 == "mo")
+    {
+        score += 12.5 ; 
+        f2 = "You got it!"; 
+        
+    }
+    if(req.query.q3 == 0)
+    {
+        score += 12.5 ; 
+        f3 = "You got it!";
+        
+    }
+    if(req.query.q4 == 0)
+    {
+        score += 12.5 ; 
+        f4 = "You got it!";
+        
+    }
+     if(req.query.q5 == 0)
+    {
+        score += 12.5 ; 
+        f5 = "You got it!";
+        
+    }
+    if(req.query.q6 == 0)
+    {
+        score += 12.5 ; 
+        f5 = "You got it!";
+        
+    }
+    if(req.query.q7 == 0)
+    {
+        score += 12.5 ; 
+        f5 = "You got it!";
+        
+    }
+    if(req.query.q8 == 0)
+    {
+        score += 12.5 ; 
+        f5 = "You got it!";
+        
+    }
+    res.send({"score":score ,"f1":f1 , "f2":f2, "f3":f3, "f4":f4, "f5":f5}); 
+    
 
+ });
 
 
 
--- public/js/functions.js
+++ public/js/functions.js
@@ -1,12 +1,24 @@
 /* global $ */
 
-if($("#totalScore").val() >= 80)
-{
-    $("#totalResponse").html("Congrats you passed!"); 
-     $("#totalResponse").css("color" , "green"); 
-}
-else if($("#totalScore").val() < 80)
-{
-    $("#totalResponse").html("Sorry you failed!"); 
-    $("#totalResponse").css("color" , "red"); 
-}
+$(document).ready(function(){
+
+  //Event Listeners
+  $(".q5choice").on("click", function()
+  {
+  	$(".q5choice").css("background-color",""); //resets background color
+  	$(this).css("background-color","rgb(255, 255, 0)"); 
+  });
+      
+      
+  if($("#totalScore").val() >= 80)
+  {
+      $("#totalResponse").html("Congrats you passed!"); 
+      $("#totalResponse").css("color" , "green"); 
+  }
+  else if($("#totalScore").val() < 80)
+  {
+      $("#totalResponse").html("Sorry you failed!"); 
+      $("#totalResponse").css("color" , "red"); 
+  }
+      
+});//end doc ready
